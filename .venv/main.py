e912b2ed0d763491ad008e6842bcef47af1c7831
ErlisonGuaraves <erlison.santos@guaraves.com.br>
2024-03-19 08:07:28 -0300

    arquivo com o condigo fonte

---
+++ .venv/main.py
@@ -0,0 +1,39 @@
+import aiohttp
+import asyncio
+import csv
+
+async def getFeriados(ano):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://api.invertexto.com/v1/holidays/{ano}?token=7033|ZVVQjtkDJ1c2ra1p34C0NMaigkQAUri7&state=PB') as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                raise Exception(f"ocorreu o seguinte erro: {response.status}") 
+
+async def buscarDados(ano, response):
+    feriados_formatados = []
+    for data_feriado in response:
+        date = data_feriado['date']
+        name = data_feriado['name']
+        type = data_feriado['type']
+        feriados_formatados.append([ano, date, name, type])  
+    return feriados_formatados
+
+async def main():
+    anos = list(range(2021, 2099+1))
+    feriados_total = []
+
+    for ano in anos:
+        data_feriado_response = await getFeriados(ano)
+        
+        if data_feriado_response:
+            feriados_formatados = await buscarDados(ano, data_feriado_response)
+            feriados_total.extend(feriados_formatados)
+
+    with open('feriados_total.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Ano', 'Data', 'Nome', 'Tipo'])
+        writer.writerows(feriados_total)
+
+if __name__ == '__main__':
+    asyncio.run(main())
